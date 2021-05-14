1d87ebf9b9a2628fd328ab0f9c98be301ac97c45
aezhe <alchar@qq.com>
2021-05-14 08:16:53 +0800

    feat(config):增加config文件

---
+++ main/miniCube_conifg.h
@@ -0,0 +1,10 @@
+#ifndef  _MINI_CUBE_CONFIG_H_
+#define  _MINI_CUBE_CONFIG_H_
+
+#define BUTTOM_POWER 23
+
+#define GPIO_MIC_WS     GPIO_NUM_32
+#define GPIO_MIC_SD     GPIO_NUM_35
+#define GPIO_MIC_SCK    GPIO_NUM_33
+
+#endif
